be7c1b8e2aec1795c0174934f5a11f9d708fc6c6
rs214 <robertsi0214@gmail.com>
2019-09-09 13:56:53 -0700

    complete react project

---
+++ src/Deck.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import Card from './Card';
+import './Deck.css';
+const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
+
+
+export default class Deck extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {deck: null, drawn: []};
+    this.getCard = this.getCard.bind(this);
+  }
+
+  async componentDidMount() {
+    let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`);
+    this.setState({deck: deck.data});
+  }
+
+  async getCard() {
+    try {
+      let res = await axios.get(`${API_BASE_URL}/${this.state.deck.deck_id}/draw/`);
+
+      if(!res.data.success) {
+        throw new Error("No cards remaining");
+      }
+
+      let card = res.data.cards[0];
+      this.setState(st => ({
+        drawn: [
+          ...st.drawn,
+          {id: card.code, image: card.image, name: `${card.value} of ${card.suit}`}
+        ]
+      }))
+    } catch(err) {
+      alert(err);
+    }
+  }
+
+  render() {
+    const cards = this.state.drawn.map(c => (
+      <Card key={c.id} name={c.name} image={c.image}/>
+      ));
+    return (
+      <div className='Deck'>
+        <h1 className='Deck-title'>Card Dealer</h1>
+        <h2 className='Deck-title subtitle'>A little demo made with React</h2>
+        <button className="Deck-btn" onClick={this.getCard}>Get Card!</button>
+        <div className='Deck-cardarea'>{cards}</div>
+      </div>
+    );
+  }
+}
